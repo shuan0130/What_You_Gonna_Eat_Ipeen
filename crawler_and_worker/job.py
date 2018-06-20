a6fedb71db9adcceb36da0d80e54cd6c3b75534a
cyh <tkionshao@gmail.com>
2018-06-20 09:23:56 +0800

    check

---
+++ crawler_and_worker/job.py
@@ -0,0 +1,9 @@
+import crawler_2
+
+_url = 'http://www.ipeen.com.tw/search/all/000/1-0-0-0/?p={}&adkw=%E5%8F%B0%E5%8C%97%E5%B8%82'
+
+path = './single_thread_restaurant_urls_{}.txt'
+
+for i in range(1,100):
+    url = _url.format(i)
+    crawler_2.get_urls.delay(url,path)
